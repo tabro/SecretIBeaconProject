d44165f6932a3bf87d50d1f30fae44cf4b3e09b4
Casper Skydt <cskydt@ebay.com>
2015-05-22 22:06:17 +0200

    Added webapi

---
+++ WebApi/WebApi/Controllers/BeaconController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class BeaconController : ApiController
+    {
+        [HttpPost]
+        [Route("Beacon")]
+        public void Post(IEnumerable<Beacon> data)
+        {
+            
+        }
+    }
+
+    public class Beacon
+    {
+        public Guid Id1 { get; set; }
+        public int Id2 { get; set; }
+        public int Id3 { get; set; }
+    }
+}
---
+++ WebApi/WebApi/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace WebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
