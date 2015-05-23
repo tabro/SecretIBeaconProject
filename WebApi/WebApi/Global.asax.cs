ee569258fd53e65167dc6dfa8fb0453ab1124362
Casper Skydt <cskydt@ebay.com>
2015-05-23 10:52:30 +0200

    Added some love

--- WebApi/WebApi/Global.asax.cs
+++ WebApi/WebApi/Global.asax.cs
@@ -1,21 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Formatting;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
+using DbUp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace WebApi
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            //const string connectionString = "Server=(local)\\SqlExpress; Database=IBeacon; Trusted_connection=true"; ;
+            const string connectionString = "Server=tcp:f1whc0l097.database.windows.net,1433;Database=IBeacon;User ID=auktioneridanmark@f1whc0l097;Password=Homer666;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;"; ;
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+            }
+
             GlobalConfiguration.Configuration.Formatters.Clear();
             GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+
         }
     }
 }
---
+++ WebApi/WebApi/Scripts/0001 CreateSchema.sql
@@ -0,0 +1,11 @@
+﻿
+create table Users(
+id int identity(1,1),
+data nvarchar(max)
+)
+
+create table Cars(
+id int identity(1,1),
+data nvarchar(max)
+)
+
