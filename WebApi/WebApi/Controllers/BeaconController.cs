5053b6e895499f7cce2b56fc2760df238af60579
Casper Skydt <cskydt@ebay.com>
2015-05-23 11:43:42 +0200

    filter beacons

--- WebApi/WebApi/Controllers/BeaconController.cs
+++ WebApi/WebApi/Controllers/BeaconController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using WebApi.Data;
 
@@ -13,7 +14,7 @@ namespace WebApi.Controllers
         [Route("Beacon")]
         public IEnumerable<Car> Post(IEnumerable<Beacon> data)
         {
-            return new GetAllCars().Get();
+            return new GetAllCars().Get().Where(c => data.Any(b => b.Equals(c.Beacon))).ToList();
         }
 
         [HttpGet]
@@ -31,8 +32,32 @@ namespace WebApi.Controllers
 
     public class Beacon
     {
+        protected bool Equals(Beacon other)
+        {
+            return Id1.Equals(other.Id1) && Id2 == other.Id2 && Id3 == other.Id3;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id1.GetHashCode();
+                hashCode = (hashCode*397) ^ Id2;
+                hashCode = (hashCode*397) ^ Id3;
+                return hashCode;
+            }
+        }
+
         public Guid Id1 { get; set; }
         public int Id2 { get; set; }
         public int Id3 { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Beacon) obj);
+        }
     }
 }
